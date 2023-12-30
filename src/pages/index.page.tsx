76caeabf118f1772ad0867f05285077a82a74d4a
Javad Tavakoli <javadtavakoli95@gmail.com>
2023-12-30 23:01:20 +0330

    used redux in dashboard

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -2,70 +2,25 @@ import List from '@/components/common/list';
 import TokenIcon from '@/components/common/token-icon';
 import React from 'react';
 import DashboardStyles from './index.styles';
+import { useAppSelector } from '@/hooks/reduxHooks';
 
 export default function Home() {
   const { Heading, Row, Name, Address, Link } = DashboardStyles;
+  const { coins } = useAppSelector((state) => state.userCoins);
   return (
     <>
       <Heading>داشبورد</Heading>
       <List>
-        {[
-          {
-            body: (
-              <Row>
-                <TokenIcon>BTC</TokenIcon>
-                <Name>بیتکوین</Name>
-                <Address>
-                  1A1zP1eP5QGefi2DMPTfT
-                  <br />
-                  L5SLmv7DivfNa
-                </Address>
-              </Row>
-            ),
-            id: 'bitcoin'
-          },
-          {
-            body: (
-              <Row>
-                <TokenIcon>BNB</TokenIcon>
-                <Name>بایننس کوین</Name>
-              </Row>
-            ),
-            id: 'binance coin'
-          },
-          {
-            body: (
-              <Row>
-                <TokenIcon>ETH</TokenIcon>
-                <Name>اتریوم</Name>
-              </Row>
-            ),
-            id: 'ethereum'
-          },
-          {
-            body: (
-              <Row>
-                <TokenIcon>SOL</TokenIcon>
-                <Name>سولانا</Name>
-              </Row>
-            ),
-            id: 'solana'
-          },
-
-          {
-            body: (
-              <Row>
-                <TokenIcon>USDT</TokenIcon>
-                <Name>تتر</Name>
-              </Row>
-            ),
-            id: 'usdt'
-          },
-          {
-            body: <Link href="/manage-tokens">+ مدیریت لیست کوین ها</Link>,
-            id: 'manage tokens'
-          }
-        ]}
+        {coins.map((coin) => ({
+          body: (
+            <Row>
+              <TokenIcon>{coin.symbol}</TokenIcon>
+              <Name>{coin.faName}</Name>
+              <Address>{coin.address}</Address>
+            </Row>
+          ),
+          id: coin.name
+        }))}
       </List>
     </>
   );
