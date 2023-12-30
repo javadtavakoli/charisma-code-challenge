54b9bfb9e299cc6bd2912d9b85f71a9dcd2f726b
Javad Tavakoli <javadtavakoli95@gmail.com>
2023-12-30 23:10:50 +0330

    added link to end of user coins list

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -7,20 +7,26 @@ import { useAppSelector } from '@/hooks/reduxHooks';
 export default function Home() {
   const { Heading, Row, Name, Address, Link } = DashboardStyles;
   const { coins } = useAppSelector((state) => state.userCoins);
+
   return (
     <>
       <Heading>داشبورد</Heading>
       <List>
-        {coins.map((coin) => ({
-          body: (
-            <Row>
-              <TokenIcon>{coin.symbol}</TokenIcon>
-              <Name>{coin.faName}</Name>
-              <Address>{coin.address}</Address>
-            </Row>
-          ),
-          id: coin.name
-        }))}
+        {coins
+          .map((coin) => ({
+            body: (
+              <Row>
+                <TokenIcon>{coin.symbol}</TokenIcon>
+                <Name>{coin.faName}</Name>
+                <Address>{coin.address}</Address>
+              </Row>
+            ),
+            id: coin.name
+          }))
+          .concat({
+            body: <Link href="/manage-tokens">+ مدیریت لیست کوین ها</Link>,
+            id: 'manage tokens'
+          })}
       </List>
     </>
   );
